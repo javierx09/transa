9a8946d752562688e1a981988754629421786044
javierx09 <javierx0962@gmail.com>
2017-11-13 09:21:45 -0300

    primer-comit
    añadido archivos principales

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<?php
+	error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+	
+	define('DBHOST', '168.232.167.26');
+	define('DBUSER', 'TRANSATRANSA');
+	define('DBPASS', 'udperritos');
+	define('DBNAME', 'dbtransa');
+	
+	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+	$dbcon = mysql_select_db(DBNAME);
+	
+	if ( !$conn ) {
+		die("Connection failed : " . mysql_error());
+	}
+	
+	if ( !$dbcon ) {
+		die("Database Connection failed : " . mysql_error());
+	}
