a63925796095aa618243b7566eeedf05e8f04ef5
javierx09 <javierx0962@gmail.com>
2017-11-13 13:35:07 -0300

    fix..

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 	ob_start();
 	session_start();
-	if( (isset($_SESSION['user'])!="")  ){
+	if( (isset($_SESSION['user'])!="") && ($_SESSION['tipo'] == 2)  ){
 		include_once 'dbconnect.php';
 
 		$error = false;
