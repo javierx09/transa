c0c4c13cf9d34409972385cc50c8accd7d226594
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 16:49:28 -0300

    ok

--- users.php
+++ users.php
@@ -5,36 +5,66 @@
 		include_once 'dbconnect.php';
 
 
+	if(isset($_POST['search']))
+	{
+	    $valueToSearch = $_POST['valueToSearch'];
+	    // search in all table columns
+	    // using concat mysql function
+	    $query = "SELECT * FROM 'users' WHERE CONCAT('id', 'userName', 'userEmail', 'TIPO') LIKE '%".$valueToSearch."%'";
+	    $search_result = filterTable($query);
+
+	}
+	 else {
+	    $query = "SELECT * FROM `users`";
+	    $search_result = filterTable($query);
+	}
+
+	// function to connect and execute the query
+	function filterTable($query)
+	{
+	    $filter_Result = mysqli_query($query);
+	    return $filter_Result;
+	}
+
 	?>
+
 	<!DOCTYPE html>
 	<html>
-	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Welcome - <?php echo $userRow['userEmail']; ?></title>
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-	<link rel="stylesheet" href="style.css" type="text/css" />
-	</head>
-	<body>
-	<div class="container">
-	<div class="row">
-        <div class="col-md-6">
-    		<h2>Custom search field</h2>
-            <div id="custom-search-input">
-                <div class="input-group col-md-12">
-									<form>
-                    <input type="text" name="busqueda" class="form-control input-lg" placeholder="Buscar" />
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-block btn-primary" name="btn-buscar">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </span>
-									</form>
-                </div>
-            </div>
-        </div>
-	</div>
-</div>
-<div class="container">
+	    <head>
+	        <title>PHP HTML TABLE DATA SEARCH</title>
+	        <style>
+	            table,tr,th,td
+	            {
+	                border: 1px solid black;
+	            }
+	        </style>
+	    </head>
+	    <body>
+
+	        <form action="php_html_table_data_filter.php" method="post">
+	            <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
+	            <input type="submit" name="search" value="Filter"><br><br>
+
+	            <table>
+	                <tr>
+	                    <th>Id</th>
+	                    <th>Nombre</th>
+	                </tr>
+
+	      <!-- populate table from mysql database -->
+	                <?php while($row = mysqli_fetch_array($search_result)):?>
+	                <tr>
+	                    <td><?php echo $row['id'];?></td>
+	                    <td><?php echo $row['fname'];?></td>
+	                    <td><?php echo $row['lname'];?></td>
+	                    <td><?php echo $row['age'];?></td>
+	                </tr>
+	                <?php endwhile;?>
+	            </table>
+	        </form>
+
+	    </body>
+	</html>
 <?php
 if ( isset($_POST['btn-buscar']) ) {
 
