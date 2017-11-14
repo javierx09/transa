b1d4837ac0842f47733d46ec605671688f6d3484
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 17:04:44 -0300

    ok

--- users.php
+++ users.php
@@ -14,12 +14,12 @@
 	    $valueToSearch = $_POST['valueToSearch'];
 	    // search in all table columns
 	    // using concat mysql function
-	    $query = "SELECT * FROM 'users' WHERE CONCAT('id', 'userName', 'userEmail', 'TIPO') LIKE '%".$valueToSearch."%'";
+	    $query = "SELECT * FROM users WHERE CONCAT('id', 'userName', 'userEmail', 'TIPO') LIKE '%".$valueToSearch."%'";
 	    $search_result = filterTable($query);
 
 	}
 	 else {
-	    $query = "SELECT * FROM `users`";
+	    $query = "SELECT * FROM users";
 	    $search_result = filterTable($query);
 	}
 
