310cdd51fd63418eafb045b9d71222d792faeaca
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 00:48:42 -0300

    ok

--- users.php
+++ users.php
@@ -16,28 +16,23 @@
 	<link rel="stylesheet" href="style.css" type="text/css" />
 	</head>
 	<body>
-		<div id="wrapper">
-
 		<div class="container">
-
-				<div class="page-header">
-				<h3>Ingrese el nombre de la persona a la que quiere buscar</h3>
-				</div>
-
-					<div class="row">
-					<div class="col-lg-12">
-					<input type="busqueda" name="search" placeholder="Search..">
-					<span class="input-group-btn">
-                        <button class="btn btn-info btn-lg" type="button">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-					</span>
-					</div>
-					</div>
-
+			<div class="row">
+		        <div class="col-md-6">
+		    		<h2>Custom search field</h2>
+		            <div id="custom-search-input">
+		                <div class="input-group col-md-12">
+		                    <input type="text" class="form-control input-lg" placeholder="Buscar" />
+		                    <span class="input-group-btn">
+		                        <button class="btn btn-info btn-lg" type="button">
+		                            <i class="glyphicon glyphicon-search"></i>
+		                        </button>
+		                    </span>
+		                </div>
+		            </div>
+		        </div>
 			</div>
-
-			</div>
+		</div>
 
 			<script src="assets/jquery-1.11.3-jquery.min.js"></script>
 			<script src="assets/js/bootstrap.min.js"></script>
