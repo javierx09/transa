bfdd48a03ef9a7d2110d0cf8c44cdc7bc5caccdf
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 01:34:12 -0300

    ok

--- users.php
+++ users.php
@@ -4,10 +4,6 @@
 	if(isset($_SESSION['user'])!="" && ($_SESSION['tipo'])==2){
 		include_once 'dbconnect.php';
 
-		$error = false;
-		$buscar = trim($_POST['busqueda']);
-		$buscar = strip_tags($buscar);
-		$buscar = htmlspecialchars($buscar);
 
 	?>
 	<!DOCTYPE html>
@@ -25,9 +21,9 @@
     		<h2>Custom search field</h2>
             <div id="custom-search-input">
                 <div class="input-group col-md-12">
-                    <input type="text" class="form-control input-lg" placeholder="Buscar" />
+                    <input type="text" name="busqueda" class="form-control input-lg" placeholder="Buscar" />
                     <span class="input-group-btn">
-                        <button class="btn btn-info btn-lg" type="button">
+                        <button type="submit" class="btn btn-info btn-lg" name="btn-buscar">
                             <i class="glyphicon glyphicon-search"></i>
                         </button>
                     </span>
@@ -36,6 +32,28 @@
         </div>
 	</div>
 </div>
+<?php
+if ( isset($_POST['btn-buscar']) ) {
+
+	$error = false;
+	$buscar = trim($_POST['busqueda']);
+	$buscar = strip_tags($buscar);
+	$buscar = htmlspecialchars($buscar);
+	$query = "SELECT * FROM users WHERE userEmail='$buscar'";
+	$result = mysql_query($query);
+	$res = mysql_query($query);
+	echo '<table>';
+for ($i = 0; $i < $max; $i++)
+{
+	echo "<PRE>";
+	print_r($res);
+	echo "</PRE>";
+}
+echo '</table>';
+
+}
+?>
+
 </body>
 </html>
 	<?php ob_end_flush();
