e6b1de8aa1e1b8c8c34d0b3d722e4ef032fb8bb1
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-13 21:35:29 -0300

    ok

--- home.php
+++ home.php
@@ -26,13 +26,13 @@
 
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+						<span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['userEmail']; ?>&nbsp;<span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="register.php">Registrar Usuario</a></li>
+								<li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
 							</ul>
 						</li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
