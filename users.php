f0a06065c842de41130e4fe805434b7bbcfcc8a6
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 17:52:57 -0300

    ok

--- users.php
+++ users.php
@@ -18,6 +18,7 @@
 	    $search_result = filterTable($query);
 			?>
 			<html>
+			<body>
 			<table>
 					<tr>
 							<th>Id</th>
@@ -35,6 +36,7 @@
 					</tr>
 					<?php endwhile;?>
 			</table>
+			</body>
 			</html>
 			<?php
 
