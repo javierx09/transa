a3c61b61ecbdadcee28bbc112d49139d47fd649d
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-14 21:53:41 -0300

    ok

--- users.php
+++ users.php
@@ -37,7 +37,7 @@
 									$valueToSearch = $_POST['valueToSearch'];
 									// search in all table columns
 									// using concat mysql function
-									$query = "SELECT * FROM users WHERE CONCAT(userId,userName,userEmail,TIPO) LIKE '%.$valueToSearch.%'";
+									$query = "SELECT * FROM users WHERE CONCAT(userId, userName, userEmail, TIPO) LIKE '%.$valueToSearch.%'";
 									$search_result = filterTable($query);
 									$count = mysql_num_rows($search_result);
 									?>
