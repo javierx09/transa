f233e69d8011fee27216cf2fda3e1359b3a3d8bf
javierx09 <javierx0962@gmail.com>
2017-11-19 03:08:32 -0300

    dsds

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
 	if( isset($_GET['edit']) )
 	{
 		$id = $_GET['edit'];
-		$res= mysql_query("SELECT * FROM users WHERE userName='$id'");
+		$res= mysql_query("SELECT * FROM users WHERE userId='$id'");
 		$row= mysql_fetch_array($res);
 	}
 
