001289305c2833ac76a539e9630cc27afe1c365c
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-19 06:42:09 -0300

    oli

--- users.php
+++ users.php
@@ -82,7 +82,7 @@
 													<td><?php echo "$row[userId]<a href='edit.php?edit1=$row[userId]'> |editar| <br />";?></td>
 													<td><?php echo "$row[userName]<a href='edit.php?edit2=$row[userName]'> |editar| <br />";?></td>
 													<td><?php if(($row['TIPO'])==2){echo "Administrador<a href='edit.php?edit3=$row[userId]'> |editar| <br />";}else{echo "Supervisor<a href='edit.php?edit3=$row[userId]'> |editar| <br />";}?></td>
-                          <td><?php echo "<a href='edit.php?edit4=$row[userId]'> dasd <br />";?></td>
+                          <td><?php echo "<a href='edit.php?edit4=$row[userId]'> vg<span class="input-group-addon"><span class="glyphicon glyphicon-erase"></span></span>";?></td>
 											</tr>
 
 											<?php endwhile;?>
