b16e6edca88d1bb0e15d6a6c54d2f4cffe6e781d
Dan chan <DanDesuu@MacBook-Pro-de-Daniela.local>
2017-11-19 06:18:29 -0300

    oli

--- edit.php
+++ edit.php
@@ -59,7 +59,7 @@
 
 	{
 		$id = $_GET['edit4'];
-		$res= mysql_query("DELETE * FROM users WHERE userId='$id'")or die("Could not update".mysql_error()); echo "<meta http-equiv='refresh' content='0;url=users.php'>";
+		$res= mysql_query("DELETE FROM users WHERE userId='$id'")or die("Could not update".mysql_error()); echo "<meta http-equiv='refresh' content='0;url=users.php'>";
 	}
 	if(isset($_POST['newsomething']))
 	{
