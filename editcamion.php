b58a7bfc134afca2c2f2c24acd69f792c348501a
javierx09 <javierx0962@gmail.com>
2017-11-19 21:48:52 -0300

    hh

--- editcamion.php
+++ editcamion.php
@@ -31,7 +31,7 @@
 		<form action="edit.php" method="POST">
 		año: <input type="text" name="newsomething" value="<?php echo $row[1]; ?>"><br />
 		<input type="hidden" name="id" value="<?php echo $row[0]; ?>">
-		<input type="hidden" name="tipo" value="año">
+		<input type="hidden" name="tipo" value="ano">
 		<input type="submit" value=" Update "/>
 		</form>
 				<?php
